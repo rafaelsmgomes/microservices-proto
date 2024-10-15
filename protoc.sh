72aa9bfca57869501b51a46ef9196d293667d144
rafaelgomes <rafael.smgomes@gmail.com>
2024-10-15 10:12:13 -0600

    changing the path to see if it works

--- protoc.sh
+++ protoc.sh
@@ -14,7 +14,7 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
-  "./$SERVICE_NAME/*.proto"
+  ./$SERVICE_NAME/*.proto
 
 cd golang/${SERVICE_NAME}
 go mod init \
