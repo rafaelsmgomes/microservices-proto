6e310182858192a8d29bb3cdd61888268edf81d9
rafaelgomes <rafael.smgomes@gmail.com>
2024-10-15 18:06:41 -0600

    small change

--- protoc.sh
+++ protoc.sh
@@ -16,6 +16,7 @@ protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
   ./$SERVICE_NAME/*.proto
 
+
 cd golang/${SERVICE_NAME}
 go mod init \
   github.com/rafaelsmgomes/microservices-proto/golang/${SERVICE_NAME} || true
